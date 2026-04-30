1c37cfe92ed58d266c03928fdf38f55fa7e86aa0
Lasiru Minruk <lasiru2225@gmail.com>
2026-04-30 15:58:49 +0530

    fix: remove hardcoded localhost URLs in admin pages
    - Replaced hardcoded `http://localhost:8000` with the dynamic `API_BASE` environment variable.
    - Fixed the fetch error that occurred when assigning user roles from `ManageTeamAdmin.jsx` and `AdminUserList.jsx` in production environments.

--- src/pages/SuperAdmin/ManageTeamAdmin/ManageTeamAdmin.jsx
+++ src/pages/SuperAdmin/ManageTeamAdmin/ManageTeamAdmin.jsx
@@ -15,6 +15,7 @@ import {
   lookupUserRequest,
   clearLookupUser,
 } from "../../../redux/userLookup/userLookupSlice";
+import { API_BASE } from "../../../utils/apiUtils";
 
 
 const initialForm = {
@@ -36,7 +37,7 @@ const MAX_CATEGORIES = 4;
 
 
 const updateUserRole = async (serviceNumber, role) => {
-  const res = await fetch("http://localhost:8000/user-role/assign", {
+  const res = await fetch(`${API_BASE}/user-role/assign`, {
     method: "POST",
     credentials: "include",
     headers: { "Content-Type": "application/json" },
